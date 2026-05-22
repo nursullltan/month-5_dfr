20d39cdd08b6d52564e09e7165ac23c13b2a82ee
Nursultan <megalodog9@gmail.com>
2026-05-22 23:51:43 +0600

    home work 7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_api', 
+        'USER': 'shop_api_user',
+        'PASSWORD': '1234', 
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
